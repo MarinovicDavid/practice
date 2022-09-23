4af13aa6d139e67ef78bfb255c8d513a655ba6be
David Marinovic <marinovic.business@gmail.com>
2022-09-23 21:55:17 +0200

    edit

--- Javascript/Grocery App/script.js
+++ Javascript/Grocery App/script.js
@@ -1,28 +1,22 @@
 //Storing elements in variables
 const list = document.getElementById('list')
 const clearList = document.getElementById('clear')
-const addItem = document.getElementById('add')
+const add = document.getElementById('add')
 const input = document.getElementById('input')
 
-//Adds functionality to "Add Item" button,
-//Checks if the item word is too short,
-//Adds the item along with plus and minus buttons,
-//Clears the input field
-addItem.addEventListener('click', () => {
-  const item = input.value
-  const itemPlus = `<button id="more">+</button>`
-  const itemMinus = `<button id="less">-</button>`
-  if (item.length < 2) {
-    alert("Item too short")
-    input.value = ''
-  } else {
-    if (list.innerHTML += `<li>${itemPlus}${item}${itemMinus}</li>`) {
-      input.value = ''
-    }
-  }
-})
+add.addEventListener('click', addItem)
 
 //Clears the entire list
-clearList.addEventListener('click', () => {
-  list.innerHTML = ''
-})
+clearList.addEventListener('click', () => { list.innerHTML = '' })
+
+//Adds a desired item on the list
+
+function addItem() {
+  const item = input.value
+  if (item.length < 2) {
+    alert("Item too short")
+  } else {
+    list.innerHTML += `<li>${item}</li>`
+  }
+  input.value = ''
+}
