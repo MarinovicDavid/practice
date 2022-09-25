2c8397d3ebf8ec25c31a8304ff6164b56790ddb7
David Marinovic <marinovic.business@gmail.com>
2022-09-25 16:08:44 +0200

    Rock Paper Scissors - code updated

--- Javascript/Rock Paper Scissors/script.js
+++ Javascript/Rock Paper Scissors/script.js
@@ -1,41 +1,53 @@
 const rock = document.getElementById('rock')
 const paper = document.getElementById('paper')
 const scissors = document.getElementById('scissors')
+const score = document.getElementById('score')
+const winner = document.getElementById('winner')
+const again = document.getElementById('again')
 
 let enemy
-
+var playerScore = 0
+var enemyScore = 0
 
 Game()
-
 
 function checkWinner() {
   if (player === "rock") {
     if (enemy === "paper") {
-      alert("You Lose!")
+      winner.innerText = `You Lose!`
+      enemyScore++
     } else if (enemy === "rock") {
-      alert("Draw!")
+      winner.innerText = `Draw!`
     } else {
-      alert("You Win!")
+      winner.innerText = `You Win!`
+      playerScore++
     }
   }
   if (player === "paper") {
     if (enemy === "scissors") {
-      alert("You Lose!")
+      winner.innerText = `You Lose!`
+      enemyScore++
     } else if (enemy === "paper") {
-      alert("Draw")
+      winner.innerText = `Draw!`
     } else {
-      alert("You Win!")
+      winner.innerText = `You Win!`
+      playerScore++
     }
   }
   if (player === "scissors") {
     if (enemy === "rock") {
-      alert("You Lose!")
+      winner.innerText = `You Lose!`
+      enemyScore++
     } else if (enemy === "paper") {
-      alert("You Win!")
+      winner.innerText = `You Win!`
+      playerScore++
     } else {
-      alert("Draw!")
+      winner.innerText = `Draw!`
     }
   }
+  gameOver()
+  inactive()
+  score.innerHTML = `<span class="player">${playerScore}</span> : <span class="enemy">${enemyScore}</span></h3>`
 }
 
 function enemyPick() {
@@ -50,20 +62,61 @@ function enemyPick() {
   return enemy
 }
 
+function inactive() {
+  if (player === "rock") {
+    paper.classList.add('inactive')
+    scissors.classList.add('inactive')
+  } else if (player === "paper") {
+    rock.classList.add('inactive')
+    scissors.classList.add('inactive')
+  } else {
+    rock.classList.add('inactive')
+    paper.classList.add('inactive')
+  }
+  again.classList.remove('inactive')
+  again.addEventListener('click', () => {
+    rock.classList.remove('inactive')
+    paper.classList.remove('inactive')
+    scissors.classList.remove('inactive')
+  })
+}
+
+function playAgain() {
+  playerScore = 0
+  enemyScore = 0
+}
+
+function gameOver() {
+  if (playerScore === 3 || enemyScore === 3) {
+    if (playerScore === 3) { winner.innerText = `Player Wins!` }
+    if (enemyScore === 3) { winner.innerText = `Enemy Wins!` }
+    rock.classList.add('inactive')
+    paper.classList.add('inactive')
+    scissors.classList.add('inactive')
+    again.classList.remove('inactive')
+    again.innerText = `New Game`
+  }
+
+}
+
 function Game() {
   rock.addEventListener('click', () => {
     player = "rock"
     enemyPick()
     checkWinner()
+
   })
   paper.addEventListener('click', () => {
     player = "paper"
     enemyPick()
     checkWinner()
+
   })
   scissors.addEventListener('click', () => {
     player = "scissors"
     enemyPick()
     checkWinner()
+
   })
+
 }
